e4b35680148ce6cd1f5722465f8ed583cecffe28
Yeferson Marcano <yefelex10@gmail.com>
2022-06-15 21:50:50 -0300

    Estructura de threejs inicial para navegación de la casa y objetos

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,53 +1,166 @@
-var scene = new THREE.Scene();
+var scene, camera, renderer, mesh;
+var meshFloor, ambientLight, light;
 
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-camera.position.z =20;
+var crate, crateTexture, crateNormalMap, crateBumpMap;
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+var keyboard = {};
+// Persona height altura del personaje, velocidad en la que camina
+var player = { height:2, speed:0.2, turnSpeed:Math.PI*0.02 };
+var USE_WIREFRAME = false;
 
-var controls = new THREE.OrbitControls(camera, renderer.domElement);
-controls.enableDamping = false;
-controls.dampingFactor = 0.25;
-controls.enableZoom = false;
+function init(){
+	scene = new THREE.Scene();
 
-var keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
-keyLight.position.set(-100, 0, 100);
+	//Perspectiva de la camara
+	camera = new THREE.PerspectiveCamera(90, 1280/720, 0.1, 1000);
+	
 
-var fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
-fillLight.position.set(100, 0, 100);
+	//Cubo rojo
+	mesh = new THREE.Mesh(
+		new THREE.BoxGeometry(1,1,1),
+		new THREE.MeshPhongMaterial({color:0xff4444, wireframe:USE_WIREFRAME})
+	);
 
-var backLight = new THREE.DirectionalLight(0xffffff, 1.0);
-backLight.position.set(100, 0, -100).normalize();
+	// Agrego un cubo en el cielo
+	mesh.position.y += 10;
+	mesh.receiveShadow = true;
+	mesh.castShadow = true;
+	scene.add(mesh);
+	
 
-scene.add(keyLight);
-scene.add(fillLight);
-scene.add(backLight);
+    // Piso  20, 20 es grande mediano...
+	meshFloor = new THREE.Mesh(
+		new THREE.PlaneGeometry(20,20, 10,10),
+		new THREE.MeshPhongMaterial({color:0xffffff, wireframe:USE_WIREFRAME})
+	);
 
-var mtlLoader = new THREE.MTLLoader();
-mtlLoader.setTexturePath('/src/img/');
-mtlLoader.setPath('/src/img/');
-mtlLoader.load('fase3.mtl', function (materials) {
 
-    materials.preload();
+	meshFloor.rotation.x -= Math.PI / 2;
+	meshFloor.receiveShadow = true;
+	scene.add(meshFloor);
+	
+	ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+	scene.add(ambientLight);
+	
+	light = new THREE.PointLight(0xffffff, 0.8, 18);
+	light.position.set(-3,6,-3);
+	light.castShadow = true;
+	light.shadow.camera.near = 0.1;
+	light.shadow.camera.far = 25;
+	scene.add(light);
+	
+	
+	var textureLoader = new THREE.TextureLoader();
+	crateTexture = textureLoader.load("src/texture/crate0_diffuse.jpg");
+	crateBumpMap = textureLoader.load("src/texture/crate0_bump.jpg");
+	crateNormalMap = textureLoader.load("src/texture/crate0_normal.jpg");
+	
 
-    var objLoader = new THREE.OBJLoader();
-    objLoader.setMaterials(materials);
-    objLoader.setPath('/src/img/');
-    objLoader.load('fase3.obj', function (object) {
+	// Caja marron
+	crate = new THREE.Mesh(
+		new THREE.BoxGeometry(3,3,3),
+		new THREE.MeshPhongMaterial({
+			color:0xffffff,
+			map:crateTexture,
+			bumpMap:crateBumpMap,
+			normalMap:crateNormalMap
+		})
+	);
+	scene.add(crate);
+	crate.position.set(-5, 3/2, -5.5);
+	crate.receiveShadow = true;
+	crate.castShadow = true;
+	// Caja marron
+	
+	// Model/material caga de casa!
+	var mtlLoader = new THREE.MTLLoader();
+	mtlLoader.load("src/models/fase4.mtl", function(materials){
+		
+		materials.preload();
+		var objLoader = new THREE.OBJLoader();
+		objLoader.setMaterials(materials);
+		
+		objLoader.load("src/models/fase4.obj", function(mesh){
+		
+			mesh.traverse(function(node){
+				if( node instanceof THREE.Mesh ){
+					node.castShadow = true;
+					node.receiveShadow = true;
+				}
+			});
+		
+			scene.add(mesh);
+			mesh.position.set(-5, 0, 4);
+			mesh.rotation.y = -Math.PI/4;
+		});
+		
+	});
+	
+	
+	// Posicion de la camara cuando carga
+	camera.position.set(0, player.height, -15);
+	camera.lookAt(new THREE.Vector3(0,player.height,0));
+	
+	renderer = new THREE.WebGLRenderer();
 
-        scene.add(object);
-        object.position.y -= 0;
+	// Elimino el scroll, no me gusta la visual
+	document.body.style.overflow = 'hidden';
 
-    });
+	// El set del maximo de la pantalla
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	// renderer.setSize(1280, 720);
 
-});
+	renderer.shadowMap.enabled = true;
+	renderer.shadowMap.type = THREE.BasicShadowMap;
+	
+	document.body.appendChild(renderer.domElement);
+	
+	animate();
+}
 
-var animate = function () {
-	requestAnimationFrame( animate );
-	controls.update();
+function animate(){
+	requestAnimationFrame(animate);
+	
+	mesh.rotation.x += 0.01;
+	mesh.rotation.y += 0.02;
+	crate.rotation.y += 0.01;
+	
+	if(keyboard[87]){ // W key
+		camera.position.x -= Math.sin(camera.rotation.y) * player.speed;
+		camera.position.z -= -Math.cos(camera.rotation.y) * player.speed;
+	}
+	if(keyboard[83]){ // S key
+		camera.position.x += Math.sin(camera.rotation.y) * player.speed;
+		camera.position.z += -Math.cos(camera.rotation.y) * player.speed;
+	}
+	if(keyboard[65]){ // A key
+		camera.position.x += Math.sin(camera.rotation.y + Math.PI/2) * player.speed;
+		camera.position.z += -Math.cos(camera.rotation.y + Math.PI/2) * player.speed;
+	}
+	if(keyboard[68]){ // D key
+		camera.position.x += Math.sin(camera.rotation.y - Math.PI/2) * player.speed;
+		camera.position.z += -Math.cos(camera.rotation.y - Math.PI/2) * player.speed;
+	}
+	
+	if(keyboard[37]){ // left arrow key
+		camera.rotation.y -= player.turnSpeed;
+	}
+	if(keyboard[39]){ // right arrow key
+		camera.rotation.y += player.turnSpeed;
+	}
+	
 	renderer.render(scene, camera);
-};
+}
 
-animate();
+function keyDown(event){
+	keyboard[event.keyCode] = true;
+}
+
+function keyUp(event){
+	keyboard[event.keyCode] = false;
+}
+
+window.addEventListener('keydown', keyDown);
+window.addEventListener('keyup', keyUp);
+
+window.onload = init;
